3f7bb5fce9cd8e901790f6da5376881e1ad051cc
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-06-09 13:49:51 -0400

    solar system render basic working

--- index.js
+++ index.js
@@ -8,7 +8,19 @@ function getSolarSystems() {
     fetch(`${BASE}/solar_systems`)
     .then(response => response.json())
     .then(solarSystems => {
-        console.log(solarSystems);
+        document.querySelector("main").innerHTML = "";
+        solarSystems.data.forEach(solarSystem => renderSolarSystem(solarSystem));
     })
+}
+
+function renderSolarSystem(solarSystem) {
+    const main = document.querySelector("main");
+    let div = document.createElement("div");
+
+    div.setAttribute("class", "ss-container");
+    div.innerHTML = `<h2>${solarSystem.attributes.name}</h2>
+    <h3>${solarSystem.attributes.star.name}</h3>`
+
+    main.appendChild(div);
 }
 
