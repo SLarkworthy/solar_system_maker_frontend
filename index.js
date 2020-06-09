2ccb9165186f4e7795d423d5a99b4d09eb1ae3fe
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-06-09 19:46:40 -0400

    submittable form saves ss to the database

--- index.js
+++ index.js
@@ -9,14 +9,26 @@ document.addEventListener('DOMContentLoaded', () => {
         const ssName = document.querySelector("#ss-name").value;
         const sunName = document.querySelector("#sun-name").value;
         const sunSpectrum = document.querySelector("#spectrum").value;
-        saveSolarSystem(ssName, sunName, sunSpectrum);
+        saveSolarSystem(ssName);
     })
 })
 
-function saveSolarSystem(ssName, sunName, sunSpectrum) {
-    console.log(ssName)
+function saveSolarSystem(ssName) {
+    fetch(`${BASE}/solar_systems`, {
+        method: 'POST',
+        headers: {"Content-Type": "application/json"},
+        body: JSON.stringify({
+            name: ssName
+        })
+    })
+    .then(response => response.json())
+    .then(solarSystem => {
+        console.log(solarSystem.id) //get ss id
+        //then fetch a single solar system
+        //then send that to render
+    })
 }
-
+ 
 
 
 function getSolarSystems() {
