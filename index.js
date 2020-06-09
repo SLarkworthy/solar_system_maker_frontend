f09b008c2183d4dd0847e60c2b26f617fc116c3f
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-06-09 17:54:46 -0400

    form event listener grabs data

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const BASE = "http://localhost:3000/api/v1"
 
 document.addEventListener('DOMContentLoaded', () => {
-    getSolarSystems()
+   
+
+    const form = document.querySelector("#new-solar-system-form");
+    form.addEventListener("submit", (e) => {
+        e.preventDefault();
+        const ssName = document.querySelector("#ss-name").value;
+        const sunName = document.querySelector("#sun-name").value;
+        const sunSpectrum = document.querySelector("#spectrum").value;
+        saveSolarSystem(ssName, sunName, sunSpectrum);
+    })
 })
+
+function saveSolarSystem(ssName, sunName, sunSpectrum) {
+    console.log(ssName)
+}
+
+
 
 function getSolarSystems() {
     fetch(`${BASE}/solar_systems`)
