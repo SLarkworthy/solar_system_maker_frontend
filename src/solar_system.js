f00adc521f880abe913565651fa436a29ddb4c50
SLarkworthy <Sarah.Larkworthy@gmail.com>
2020-06-10 16:34:30 -0400

    OOJS refactor index working

---
+++ src/solar_system.js
@@ -0,0 +1,22 @@
+class SolarSystem {
+    constructor(solarSystem) {
+        this.id = solarSystem.id;
+        this.name = solarSystem.attributes.name;
+        this.star = solarSystem.attributes.star;
+        SolarSystem.all.push(this);
+    }
+
+    renderSolarSystem() {
+        const main = document.querySelector("main");
+        let div = document.createElement("div");
+    
+        div.setAttribute("class", "ss-container");
+        div.setAttribute("data-id", this.id);
+        div.innerHTML = `<h2>${this.name}</h2>
+        <h3>${this.star.name}</h3>`
+    
+        main.appendChild(div);
+    }
+}
+
+SolarSystem.all = [];
